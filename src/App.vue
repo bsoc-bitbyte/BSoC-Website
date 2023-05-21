ab998df2d5444d8b7c2ab44e2acaa702edb6c6b3
Jay <jai3500000@gmail.com>
2023-05-22 00:34:05 +0530

    home page added

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
-  <PreLoader v-if="!spin"/>
-  <router-view v-if="spin"/>
+  <PreLoader v-if="!spin" />
+  <router-view v-if="spin" />
 </template>
 
 <style>
@@ -11,18 +11,17 @@
   font-family: 'Stargaze';
   src: url('./assets/fonts/StargazeStencil.otf');
 }
-
 </style>
 <script>
 import PreLoader from "@/components/PreLoader";
-import {ref} from "vue";
+import { ref } from "vue";
 
 export default {
-  components: {PreLoader},
+  components: { PreLoader },
   setup() {
     const spin = ref(false)
     setTimeout(() => spin.value = true, 3000)
-    return {spin}
+    return { spin }
   }
 }
 </script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,60 +1,64 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import LandingPage from "@/views/LandingPage.vue";
+import { createRouter, createWebHistory } from "vue-router";
+import LandingPage from "@/views/LandingPage";
 import Auth from "@/views/Auth";
-import {projectAuth} from "@/firebase/config";
+import { projectAuth } from "@/firebase/config";
 import Dashboard from "@/views/Dashboard";
 import SubmitPR from "@/views/SubmitPR";
-
+import Home from "@/views/Home";
 
 const requireAuth = (to, from, next) => {
-  let user = projectAuth.currentUser
-  if(!user) {
-    next({path: "/auth"})
+  let user = projectAuth.currentUser;
+  if (!user) {
+    next({ path: "/auth" });
+  } else {
+    next();
   }
-  else {
-    next()
-  }
-}
+};
 
 const requireNoAuth = (to, from, next) => {
-  let user = projectAuth.currentUser
-  if(user) {
-    next({path: "/dashboard"})
+  let user = projectAuth.currentUser;
+  if (user) {
+    next({ path: "/dashboard" });
+  } else {
+    next();
   }
-  else {
-    next()
-  }
-}
+};
 
 const routes = [
   {
-    path: '/',
-    name: 'LandingPage',
-    component: LandingPage
+    path: "/",
+    name: "LandingPage",
+    component: LandingPage,
   },
   {
-    path: '/auth',
-    name: 'Auth',
+    path: "/auth",
+    name: "Auth",
     component: Auth,
-    beforeEnter: requireNoAuth
+    beforeEnter: requireNoAuth,
   },
   {
-    path: '/dashboard',
-    name: 'Dashboard',
+    path: "/dashboard",
+    name: "Dashboard",
     component: Dashboard,
-    beforeEnter: requireAuth
+    beforeEnter: requireAuth,
   },
   {
-    path: '/submit',
-    name: 'Submit',
+    path: "/submit",
+    name: "Submit",
     component: SubmitPR,
-    beforeEnter: requireAuth
-  }
-]
+    beforeEnter: requireAuth,
+  },
+  {
+    path: "/home",
+    name: "Home",
+    component: Home,
+    beforeEnter: requireNoAuth,
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
