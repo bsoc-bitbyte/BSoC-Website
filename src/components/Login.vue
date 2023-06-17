f6b2c8535244a57f8f2ccee19d9d9da15e3852e0
Kushagra <22bcs148@iiitdmj.ac.in>
2023-06-18 00:47:32 +0530

    feat: made the login and signup page responsive

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,35 +1,41 @@
 <template>
-      <div class="screen" > 
+  <div class="screen">
+    <div class="card bg-dark text-white">
+      <span v-on:click="toggle()" class="x" style="position: relative;
+    margin-left: 84%;
+    padding-top: 1em;">x</span>
+      <div class="row d-flex justify-content-center align-items-center h-100" style="margin-top: 1em">
+        <div class="card-body px-5 text-center">
+          <div class="">
+            <h2 class="fw-bold mb-2 text-uppercase">Login</h2>
+            <p class="text-white-50 mb-5">
+              Please enter your login and password!
+            </p>
 
-      <div class="card bg-dark text-white"  style="background-color: blue; border-radius: 1rem;">
-        <span v-on:click="toggle()"  class="x" style="position: relative; margin-left: 15em; padding-top: 1em;">x</span>
-        <div class="row d-flex justify-content-center align-items-center h-100" style="margin-top: -1em; ">
-      <div class="card-body p-5 text-center" >
-            <div class="mb-md-5 mt-md-4 pb-5">
+            <div class="form-outline form-white mb-4">
+              <input type="email" id="typeEmailX" class="form-control form-control-lg" placeholder="Email"
+                v-model="email" />
+            </div>
 
-              <h2 class="fw-bold mb-2 text-uppercase">Login</h2>
-              <p class="text-white-50 mb-5">Please enter your login and password!</p>
+            <div class="form-outline form-white mb-4">
+              <input type="password" id="typePasswordX" class="form-control form-control-lg" placeholder="Password"
+                v-model="password" />
+            </div>
 
-              <div class="form-outline form-white mb-4">
-                <input type="email" id="typeEmailX" class="form-control form-control-lg" placeholder="Email" v-model="email"/>
-               
-              </div>
-
-              <div class="form-outline form-white mb-4">
-                <input type="password" id="typePasswordX" class="form-control form-control-lg " placeholder="Password" v-model="password" />
-                
-              </div>
-
-
-              <button class="btn btn-outline-light tn-lg px-5" type="submit" @click="handleSubmit">Login</button>
-              <div class="d-flex flex-column mt-4 pt-1">
-                <span>or</span>
+            <button class="btn btn-outline-light tn-lg px-5" type="submit" @click="handleSubmit">
+              Login
+            </button>
+            <div class="d-flex flex-column mt-4 pt-1">
+              <span>or</span>
               <div class="d-flex justify-content-center text-center mt-4 pt-1" @click="handleGoogleSubmit">
-                <a href="#!" class="text-white"><img src="../assets/google-icon.png" style="height: 1.5em;"/></a>
-              </div></div>
-
-            </div></div></div></div>
-</div>
+                <a href="#!" class="text-white"><img src="../assets/google-icon.png" style="height: 1.5em" /></a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -39,72 +45,79 @@ import useSignInGoogle from "../composables/useSignInGoogle";
 
 export default {
   name: "Login",
-  props: ['toggle'], 
+  props: ["toggle"],
   setup(props, context) {
-    const { error, login } = useLogin()
+    const { error, login } = useLogin();
 
-    const email = ref("")
-    const password = ref("")
-    const loading = ref(false)
+    const email = ref("");
+    const password = ref("");
+    const loading = ref(false);
 
     const handleSubmit = async () => {
-      loading.value = true
-      await login(email.value, password.value)
-      loading.value = false
+      loading.value = true;
+      await login(email.value, password.value);
+      loading.value = false;
       if (!error.value) {
-        context.emit("login")
+        context.emit("login");
       }
-    }
-    
+    };
 
-    const { err, googleLogin } = useSignInGoogle()
+    const { err, googleLogin } = useSignInGoogle();
     const handleGoogleSubmit = async () => {
-      await googleLogin()
+      await googleLogin();
       if (!err.value) {
-        console.log("Auth Complete")
-        context.emit("login")
+        console.log("Auth Complete");
+        context.emit("login");
       } else {
-        console.log(err.value)
+        console.log(err.value);
       }
-    }
+    };
 
     const handleClick = () => {
-      context.emit("toggleAuth")
-    }
+      context.emit("toggleAuth");
+    };
 
-    return { email, password, loading, handleSubmit, handleGoogleSubmit, handleClick }
-  }
-}
+    return {
+      email,
+      password,
+      loading,
+      handleSubmit,
+      handleGoogleSubmit,
+      handleClick,
+    };
+  },
+};
 </script>
 
 <style scoped>
-@import url('https://fonts.googleapis.com/css?family=Raleway:400,700');
-
+@import url("https://fonts.googleapis.com/css?family=Raleway:400,700");
 
 .screen {
   background: rgb(19, 18, 17);
   position: absolute;
-	top: calc(100%);
-	left: calc(42%);
-  z-index: 2;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, 4%);
+  transition: all 0.3s ease;
   filter: blur(px);
-  height: 580px;
-  width: 400px;
+  height: 84vh;
+  width: 28vw;
   box-shadow: 0px 0px 24px black;
-  font-family: popins , sans-serif;
-  
+  font-family: popins, sans-serif;
 }
- 
-  
-.container{
-  position: relative;
-  
 
-  
+.card {
+  height: 84vh;
+  width: 28vw;
+  margin-left: auto;
+  margin-right: auto;
+  background-color: blue;
+  border-radius: 1rem;
 }
-.x{
+
+.x {
   position: relative;
-  
+
   /* top: 2em; */
   border: none;
   background-color: transparent;
@@ -112,15 +125,42 @@ export default {
   font-size: x-large;
   cursor: pointer;
 }
-@media (max-width:768px){
 
-  .screen{
-    margin-top: 0%;
-    left: 0%;
-
+@media (max-width: 1325px) {
+  .x {
+    width: 400px;
   }
-  .x{
-    left: -4em;
+
+  .card {
+    height: 84vh;
+    width: 400px;
+    margin-right: 300px;
+    background-color: blue;
+    border-radius: 1rem;
+  }
+
+  .screen {
+    width: 400px;
+    margin-right: 300px;
+  }
+}
+
+@media (max-width: 768px) {
+  .x {
+    width: 200px;
+  }
+
+  .card {
+    height: 84vh;
+    width: 300px;
+    margin-right: 300px;
+    background-color: blue;
+    border-radius: 1rem;
+  }
+
+  .screen {
+    width: 300px;
+    margin-right: 300px;
   }
 }
 </style>
--- src/components/Signup.vue
+++ src/components/Signup.vue
@@ -1,37 +1,70 @@
 <template>
   <div class="content">
     <div class="screen">
-      <div class="card bg-dark text-white" style="border-radius: 1rem;  ">
-        <span v-on:click="toggle()"  class="x" style="position: relative; margin-left: 15em; padding-top: 1em;">x</span>
-        <div class="row d-flex justify-content-center align-items-center h-100" style="margin-top: -1em; ">
-      <div class="card-body p-5 text-center" >
-            <div class="mb-md-5 mt-md-4 pb-5">
+    
+        <div class="card bg-dark text-white">
+        <span v-on:click="toggle()" class="x" style="">x</span>
+          <div
+            class="row d-flex justify-content-center align-items-center h-100"
+            style="margin-top: -1em">
+            <div class="card-body px-5 text-center">
+              <div class="">
+                <h2 class="fw-bold mb-2 text-uppercase">Sign Up</h2>
+                <p class="text-white-50 mb-4">Please enter email and password!</p>
+                <div class="form-outline form-white mb-4">
+                  <input
+                    type="name"
+                    id="typeEmailX"
+                    class="form-control form-control-lg"
+                    placeholder="Name"
+                    v-model="displayName"
+                  />
+                </div>
+                <div class="form-outline form-white mb-4">
+                  <input
+                    type="email"
+                    id="typeEmailX"
+                    class="form-control form-control-lg"
+                    placeholder="Email"
+                    v-model="email"
+                  />
+                </div>
 
-              <h2 class="fw-bold mb-2 text-uppercase">Sign Up</h2>
-              <p class="text-white-50 mb-5">Please enter email and password!</p>
-              <div class="form-outline form-white mb-4">
-                <input type="name" id="typeEmailX" class="form-control form-control-lg" placeholder="Name" v-model="displayName"/>
+                <div class="form-outline form-white mb-4">
+                  <input
+                    type="password"
+                    id="typePasswordX"
+                    class="form-control form-control-lg"
+                    placeholder="Password"
+                    v-model="password"
+                  />
+                </div>
+
+                <button
+                  class="btn btn-outline-light tn-lg px-5"
+                  type="submit"
+                  @click="handleSubmit"
+                >
+                  Sign Up
+                </button>
+                <div class="d-flex flex-column mt-2 pt-1">
+                  <span>or</span>
+                  <div
+                    class="d-flex justify-content-center text-center mt-2 pt-1"
+                    @click="handleGoogleSubmit"
+                  >
+                    <a href="#!" class="text-white"
+                      ><img src="../assets/google-icon.png" style="height: 1.5em"
+                    /></a>
+                  </div>
+                </div>
               </div>
-              <div class="form-outline form-white mb-4">
-                <input type="email" id="typeEmailX" class="form-control form-control-lg" placeholder="Email" v-model="email"/>
-              </div>
-
-              <div class="form-outline form-white mb-4">
-                <input type="password" id="typePasswordX" class="form-control form-control-lg " placeholder="Password" v-model="password" />
-              </div>
-
-
-              <button class="btn btn-outline-light tn-lg px-5" type="submit" @click="handleSubmit">Sign Up</button>
-              <div class="d-flex flex-column mt-4 pt-1">
-                <span>or</span>
-              <div class="d-flex justify-content-center text-center mt-4 pt-1" @click="handleGoogleSubmit">
-                <a href="#!" class="text-white"><img src="../assets/google-icon.png" style="height: 1.5em;"/></a>
-              </div></div>
-
-            </div></div></div></div>
+            </div>
+          </div>
+        </div>
     </div>
   </div>
- 
+
 </template>
 
 <script>
@@ -43,92 +76,142 @@ import addUsers from "@/composables/addUsers";
 
 export default {
   name: "Signup",
-  props: ['toggle'],
+  props: ["toggle"],
   setup(props, context) {
+    const displayName = ref("");
+    const email = ref("");
+    const password = ref("");
+    const loading = ref(null);
 
-    const displayName = ref("")
-    const email = ref("")
-    const password = ref("")
-    const loading = ref(null)
-
-    const { error, signup } = useSignup()
-    const { e, addDoc } = addUsers("Users")
+    const { error, signup } = useSignup();
+    const { e, addDoc } = addUsers("Users");
 
     const handleSubmit = async () => {
-      loading.value = true
-      await signup(email.value, password.value, displayName.value)
+      loading.value = true;
+      await signup(email.value, password.value, displayName.value);
       if (!error.value) {
-        loading.value = false
-        const user = projectAuth.currentUser
-        const data = user.providerData[0]
-        await addDoc({ ...data, score: 0 })
-        context.emit('login')
+        loading.value = false;
+        const user = projectAuth.currentUser;
+        const data = user.providerData[0];
+        await addDoc({ ...data, score: 0 });
+        context.emit("login");
       }
-    }
+    };
 
-    const { err, googleLogin } = useSignInGoogle()
+    const { err, googleLogin } = useSignInGoogle();
 
     const handleGoogleSubmit = async () => {
-      await googleLogin()
+      await googleLogin();
       if (!err.value) {
-        const user = projectAuth.currentUser
-        const data = user.providerData[0]
-        await addDoc({ ...data, score: 0 })
-        context.emit('login')
+        const user = projectAuth.currentUser;
+        const data = user.providerData[0];
+        await addDoc({ ...data, score: 0 });
+        context.emit("login");
       } else {
-        console.log(err.value)
+        console.log(err.value);
       }
-    }
+    };
 
     const handleClick = () => {
-      context.emit("toggleAuth")
-    }
+      context.emit("toggleAuth");
+    };
 
-
-    return { displayName, email, password, handleSubmit, loading, handleGoogleSubmit, handleClick }
-  }
-}
+    return {
+      displayName,
+      email,
+      password,
+      handleSubmit,
+      loading,
+      handleGoogleSubmit,
+      handleClick,
+    };
+  },
+};
 </script>
 
 <style scoped>
-@import url('https://fonts.googleapis.com/css?family=Raleway:400,700');
+@import url("https://fonts.googleapis.com/css?family=Raleway:400,700");
 
 
+.parent {
+  margin:0;
+  padding: 0;
+  display: flex;
+  justify-content: center;
+}
 .screen {
   background: rgb(19, 18, 17);
   position: absolute;
-	top: calc(100%);
-	left: calc(42%);
-  z-index: 2;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, 4%);
+  transition: all 0.3s ease;
   filter: blur(px);
-  height: 640px;
-  width: 400px;
+  height: 84vh;
+  width: 33vw;
   box-shadow: 0px 0px 24px black;
-  font-family: popins , sans-serif;
-  
+  font-family: popins, sans-serif;
 }
-.x{
-  position: relative;
-  
+
+.card {
+  border-radius: 1rem;
+  height: 84vh;
+  width: 33vw;
+  margin-left: auto;
+  margin-right: auto;
+  background-color: blue;
+  border-radius: 1rem;
+}
+
+.x {
   /* top: 2em; */
+  position: relative;
+  margin-left: 84%;
+  padding-top: 1em;
   border: none;
   background-color: transparent;
   font-weight: bold;
   font-size: x-large;
   cursor: pointer;
 }
+/* @media (max-width: 1325px) {
+  .x {
+    width: 400px;
+  }
+  .card {
+    border-radius: 1rem;
+    height: 84vh;
+    width: 400px;
+    margin-right: 300px;
+    background-color: blue;
+    border-radius: 1rem;
+  } 
 
-@media (max-width:768px){
-
-.screen{
-  margin-top: 0%;
-  left: 0%;
-
+  .screen {
+    width: 400px;
+    margin-right: 300px;
+  }
 }
-.x{
-  left: -4em;
-}
-}
 
+@media (max-width: 768px) {
+  .x {
+    position: relative;
+    width: 20px;
+  }
+  .card {
+    border-radius: 1rem;
+    height: 84vh;
+    width: 300px;
+    margin-right: 300px;
+    margin-left: 0px;
+    background-color: blue;
+    border-radius: 1rem;
+  }
 
+  .screen {
+    width: 300px;
+    margin-right: 300px;
+    margin-left: 0%;
+  }
+}*/
 </style>
