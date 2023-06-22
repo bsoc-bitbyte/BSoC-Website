5e88e8d27e81190d59e4863cb2ec47607a5e5986
Priyansh61 <21bec080@iiitdmj.ac.in>
2023-06-22 12:11:25 +0530

    Added user stats get function

--- src/composables/getCollection.js
+++ src/composables/getCollection.js
@@ -27,4 +27,58 @@ const getCollection = (collection) => {
     return {error, documents}
 }
 
-export default getCollection
+const getAllUserStats = (collection) => {
+    const documents = ref(null)
+    const error = ref(null)
+
+    console.log("in get all user stats",collection,uid)
+
+    let collectionRef = projectFirestore.collection(collection);
+    const populate = collectionRef.onSnapshot((snap) => {
+        let results = []
+        snap.docs.forEach(doc => {
+            results.push({ ...doc.data(), id: doc.id, time_sec: doc.time })
+        })
+        results.sort((a, b) => (a.score < b.score) ? 1 : -1)
+        documents.value = results
+        error.value = null
+    }, (err) => {
+        console.log(err.message)
+        documents.value = null
+        error.value = "error in fetching user stats"
+    });
+
+    watchEffect((onInvalidate) => {
+        onInvalidate(() => populate())
+    })
+
+    return {error, documents}
+}
+
+const getSingleUserStats = async (collection, uid) => {
+    const documents = ref(null);
+    const error = ref(null);
+  
+    try {
+      const collectionRef = projectFirestore.collection(collection).where("uid", "==", uid);
+      const snap = await collectionRef.get();
+      const results = [];
+  
+      snap.forEach(doc => {
+        results.push({ ...doc.data(), id: doc.id, time_sec: doc.time });
+      });
+  
+      results.sort((a, b) => (a.score < b.score) ? 1 : -1);
+      documents.value = results;
+      error.value = null;
+    } catch (err) {
+      console.log(err.message);
+      documents.value = null;
+      error.value = "error in fetching user stats";
+    }
+  
+    return { error, documents };
+  };
+
+
+export  {getCollection, getAllUserStats, getSingleUserStats};
