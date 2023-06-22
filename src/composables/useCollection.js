46d384fbfb9b3c5177240e4b75ae5ddb93ca4f6d
Priyansh61 <21bec080@iiitdmj.ac.in>
2023-06-22 12:12:15 +0530

    Added users stats add function

--- src/composables/useCollection.js
+++ src/composables/useCollection.js
@@ -1,21 +1,65 @@
-import {ref} from "vue";
-import {projectFirestore} from "@/firebase/config";
+import { ref } from "vue";
+import { projectFirestore } from "@/firebase/config";
+import { getDoc,setDoc,doc as firebaseDoc } from "firebase/firestore"; 
 
-const useCollection = (collection) => {
-    const error = ref(null)
 
-    const addDoc = async (doc) => {
-        error.value = null
 
-        try {
-            await projectFirestore.collection(collection).add(doc)
-        } catch (err) {
-            console.log(err.message)
-            error.value = "Could not send message!!"
-        }
+const addDoc = async (collection,doc) => {
+    const error = ref(null);
+
+    try {
+        await projectFirestore.collection(collection).add(doc)
+    } catch (err) {
+        console.log(err.message)
+        error.value = "Could not send message!!"
     }
-
-    return {error, addDoc}
 }
 
-export default useCollection
+const addUserStats = async (collection, doc) => {
+    const error = ref(null)
+
+    try {
+        await projectFirestore.collection(collection).add(doc)
+    } catch (err) {
+        console.log(err.message)
+        error.value = "Could not add UserStats!!"
+    }
+}
+
+const updateUserStats = async (collection, doc, id) => {
+    try {
+        const docRef = firebaseDoc(projectFirestore, collection, id);
+        const existingDoc = await getDoc(docRef);
+        let updatedScore = 0;
+        if (!existingDoc.exists()) {
+            console.log("No such document!");
+            updatedScore = doc.difficulty;
+            await setDoc(docRef, {
+                message: doc.message,
+                link: doc.link,
+                score: updatedScore,
+                displayName: doc.displayName,
+                time: doc.time
+            }, { merge: true });
+            console.log("Document successfully written!","score updated",updatedScore)
+            return;
+        }
+        const existingScore = existingDoc.data().score;
+        updatedScore = parseInt(existingScore) + parseInt(doc.difficulty);
+
+        await setDoc(docRef, {
+            message: doc.message,
+            link: doc.link,
+            score: updatedScore,
+            displayName: doc.displayName,
+            time: doc.time
+        }, { merge: true });
+
+        console.log("Document successfully written!","score updated",updatedScore)
+    } catch (err) {
+        console.log(err.message);
+    }
+}
+
+
+export { addDoc, addUserStats, updateUserStats }
