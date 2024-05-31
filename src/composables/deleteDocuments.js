4f934302270bc053b4b99be49a9f16cc24dcd7e1
ramG-reddy <codewizz.162111@gmail.com>
2024-05-31 23:37:24 +0530

    fix: Changed All DB collections to new ones

--- src/composables/deleteDocuments.js
+++ src/composables/deleteDocuments.js
@@ -1,84 +1,95 @@
-import {ref} from "vue";
-import {projectFirestore} from "@/firebase/config";
-import {getCollection} from "@/composables/getCollection";
+import { ref } from 'vue'
+import { projectFirestore } from '@/firebase/config'
+import { getCollection } from '@/composables/getCollection'
 
 const deleteDocuments = async (n) => {
-    const { err, documents } = getCollection("PriorityQueue")
-    const colRef = await projectFirestore.collection("PriorityQueue")
-    for(let i = 0; i < n && i < docs.value.length; i++) {
-        const docRef = colRef.doc(docs[i].id)
-        docRef.delete().then(() => {
-            console.log(docRef + " deleted")
-        }).catch((e) => {
-            console.log(e.message)
-        })
-    }
-    console.log("finished deleting")
+	const { err, documents } = getCollection('PriorityQueue')
+	const colRef = await projectFirestore.collection('PriorityQueue')
+	for (let i = 0; i < n && i < docs.value.length; i++) {
+		const docRef = colRef.doc(docs[i].id)
+		docRef
+			.delete()
+			.then(() => {
+				console.log(docRef + ' deleted')
+			})
+			.catch((e) => {
+				console.log(e.message)
+			})
+	}
+	console.log('finished deleting')
 }
 
 const deletePr = async (id, doc, difficulty, uid) => {
+	if (
+		doc.id == id &&
+		confirm(
+			'Are you sure you want to delete ? \nThis action can not be undone.'
+		)
+	) {
+		const colRef = await projectFirestore.collection('dashboard-2024')
+		const docRef = colRef.doc(doc.id)
+		docRef
+			.delete()
+			.then(() => {})
+			.catch((e) => {
+				document.querySelector('.toaster .card').style.backgroundColor =
+					'#dc3545db'
+				document.querySelector('.toaster .card-title').innerHTML = 'Error !'
+				document.querySelector('.toaster .card-text').innerHTML = e.message
+			})
 
-        if (doc.id == id && confirm("Are you sure you want to delete ? \nThis action can not be undone.")){
-            const colRef = await projectFirestore.collection("dashboard-2023")
-            const docRef = colRef.doc(doc.id)
-            docRef.delete().then(() => {                
-            }).catch((e) => {
-                document.querySelector(".toaster .card").style.backgroundColor = '#dc3545db'
-                document.querySelector(".toaster .card-title").innerHTML = "Error !"
-                document.querySelector(".toaster .card-text").innerHTML = e.message
-            })
-            
+		var current_score = 0
+		var new_score = 0
+		var current_prs = 0
+		var new_prs = 0
 
-            var current_score = 0
-            var new_score = 0
-            var current_prs = 0
-            var new_prs = 0
-            
-            
-            await projectFirestore.collection("userStats-2023").doc(uid).get().then((snapshot) => {
-            current_score = snapshot.data().score
-            current_prs = snapshot.data().numberOfPRs
-            new_score = current_score-difficulty
-            new_prs = current_prs-1
-            
-            }).catch((e) => {
-                document.querySelector(".toaster .card").style.backgroundColor = '#dc3545db'
-                document.querySelector(".toaster .card-text").innerHTML = e.message
-                document.querySelector(".toaster .card-title").innerHTML = "Error !"
-            })
+		await projectFirestore
+			.collection('userStats-2024')
+			.doc(uid)
+			.get()
+			.then((snapshot) => {
+				current_score = snapshot.data().score
+				current_prs = snapshot.data().numberOfPRs
+				new_score = current_score - difficulty
+				new_prs = current_prs - 1
+			})
+			.catch((e) => {
+				document.querySelector('.toaster .card').style.backgroundColor =
+					'#dc3545db'
+				document.querySelector('.toaster .card-text').innerHTML = e.message
+				document.querySelector('.toaster .card-title').innerHTML = 'Error !'
+			})
 
-            await projectFirestore.collection("userStats-2023").doc(uid).update({score: new_score, numberOfPRs: new_prs});
-            
-            function show_toast() { 
-               document.querySelector(".toaster").style.display = "flex"                                           
-            }
+		await projectFirestore
+			.collection('userStats-2024')
+			.doc(uid)
+			.update({ score: new_score, numberOfPRs: new_prs })
 
-            function fade(element) {
-                if (element != undefined){
-                    var op = 1;
-                    var timer = setInterval(function () {
-                        if (op <= 0.1){
-                            clearInterval(timer);
-                            element.style.display = 'none';
-                        }
-                        element.style.opacity = op;
-                        element.style.filter = 'alpha(opacity=' + op * 100 + ")";
-                        op -= op * 0.1;
-                    }, 50);
-                }
-            }
+		function show_toast() {
+			document.querySelector('.toaster').style.display = 'flex'
+		}
 
+		function fade(element) {
+			if (element != undefined) {
+				var op = 1
+				var timer = setInterval(function () {
+					if (op <= 0.1) {
+						clearInterval(timer)
+						element.style.display = 'none'
+					}
+					element.style.opacity = op
+					element.style.filter = 'alpha(opacity=' + op * 100 + ')'
+					op -= op * 0.1
+				}, 50)
+			}
+		}
 
-            show_toast();
+		show_toast()
 
-            setTimeout(() => {
-                fade(document.querySelector(".toaster"))
-            }, 3000)
-            
-            
-        }       
-
-
+		setTimeout(() => {
+			fade(document.querySelector('.toaster'))
+		}, 3000)
+	}
 }
 
-export {deleteDocuments, deletePr}
+export { deleteDocuments, deletePr }
--- src/views/SubmitPR.vue
+++ src/views/SubmitPR.vue
@@ -1,160 +1,184 @@
 <template>
-  <div class="content">
-    <div class="inner">
-      <input class="field" type="text" placeholder="Commit Message" v-model="message" required autofocus>
-      <input class="field" type="url" placeholder="Link to PR" v-model="link" required>
-      <select class="custom-select select" v-model="difficulty" id="inputGroupSelect01"  required>
-        <option selected>Select Difficulty</option>
-        <option value="15">Easy</option>
-        <option value="30">Medium</option>
-        <option value="50">Difficult</option>
-      </select>
-      <button v-if="!loading" class="hvr-grow" @click="handleClick">Submit</button>
-      <button v-else disabled class="disabled">Submitting</button>
-    </div>
-  </div>
+	<div class="content">
+		<div class="inner">
+			<input
+				class="field"
+				type="text"
+				placeholder="Commit Message"
+				v-model="message"
+				required
+				autofocus
+			/>
+			<input
+				class="field"
+				type="url"
+				placeholder="Link to PR"
+				v-model="link"
+				required
+			/>
+			<select
+				class="custom-select select"
+				v-model="difficulty"
+				id="inputGroupSelect01"
+				required
+			>
+				<option selected>Select Difficulty</option>
+				<option value="15">Easy</option>
+				<option value="30">Medium</option>
+				<option value="50">Difficult</option>
+			</select>
+			<button v-if="!loading" class="hvr-grow" @click="handleClick">
+				Submit
+			</button>
+			<button v-else disabled class="disabled">Submitting</button>
+		</div>
+	</div>
 </template>
 
 <script>
-import { ref } from "vue";
-import { projectAuth } from "@/firebase/config";
-import {addDoc,updateUserStats, addUserStats} from "../composables/useCollection";
-import { timestamp } from "@/firebase/config";
-import { useRouter } from "vue-router";
+import { ref } from 'vue'
+import { projectAuth } from '@/firebase/config'
+import {
+	addDoc,
+	updateUserStats,
+	addUserStats,
+} from '../composables/useCollection'
+import { timestamp } from '@/firebase/config'
+import { useRouter } from 'vue-router'
 
 export default {
-  name: "SubmitPR",
-  setup() {
-    const message = ref("");
-    const link = ref("");
-    const difficulty = ref("Select Difficulty");
-    const loading = ref(false)
-    const displayName = projectAuth.currentUser.displayName
-    const router = useRouter();
+	name: 'SubmitPR',
+	setup() {
+		const message = ref('')
+		const link = ref('')
+		const difficulty = ref('Select Difficulty')
+		const loading = ref(false)
+		const displayName = projectAuth.currentUser.displayName
+		const router = useRouter()
 
-    const handleClick = async () => {
+		const handleClick = async () => {
+			if (difficulty.value === 'Select Difficulty') {
+				alert('Please select a difficulty')
+				return
+			}
 
-      if (difficulty.value === "Select Difficulty") {
-        alert("Please select a difficulty")
-        return
-      }
+			if (message.value === '' || link.value === '') {
+				alert('Please fill in all fields')
+				return
+			}
+			loading.value = true
 
-      if (message.value === "" || link.value === "") {
-        alert("Please fill in all fields")
-        return
-      }
-      loading.value = true
+			const doc = {
+				message: message.value,
+				link: link.value,
+				difficulty: difficulty.value,
+				displayName: displayName,
+				time: timestamp(),
+				uid: projectAuth.currentUser.uid,
+			}
 
-      const doc = {
-        message: message.value,
-        link: link.value,
-        difficulty: difficulty.value,
-        displayName: displayName,
-        time: timestamp(),
-        uid: projectAuth.currentUser.uid
-      }
+			await updateUserStats('userStats-2024', doc, projectAuth.currentUser.uid)
 
-      await updateUserStats("userStats-2023",doc,projectAuth.currentUser.uid);
-      
-      console.log(doc)
-      await addDoc("dashboard-2023", doc);
-      loading.value = false
+			console.log(doc)
+			await addDoc('dashboard-2024', doc)
+			loading.value = false
 
-      await router.push("/dashboard")
-    }
+			await router.push('/dashboard')
+		}
 
-    return { message, link, handleClick, loading, difficulty }
-  }
+		return { message, link, handleClick, loading, difficulty }
+	},
 }
 </script>
 
 <style scoped>
 .content {
-  height: 100vh;
-  width: 100vw;
+	height: 100vh;
+	width: 100vw;
 
-  display: flex;
-  justify-content: center;
-  align-items: center;
+	display: flex;
+	justify-content: center;
+	align-items: center;
 
-  background: #19192a;
+	background: #19192a;
 }
 
 .inner {
-  width: 40vw;
-  min-width: 550px;
-  height: 42vh;
+	width: 40vw;
+	min-width: 550px;
+	height: 42vh;
 
-  margin: 40px 0;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-evenly;
-  align-items: center;
+	margin: 40px 0;
+	display: flex;
+	flex-direction: column;
+	justify-content: space-evenly;
+	align-items: center;
 
-  background: #19192a;
-  box-shadow: 4px 4px 40px 2px #132e72;
-  border-radius: 8px;
+	background: #19192a;
+	box-shadow: 4px 4px 40px 2px #132e72;
+	border-radius: 8px;
 }
 
-.inner input, .select {
-  width: 90%;
+.inner input,
+.select {
+	width: 90%;
 
-  border: none;
-  box-sizing: border-box;
-  border-radius: 8px;
-  outline: none;
+	border: none;
+	box-sizing: border-box;
+	border-radius: 8px;
+	outline: none;
 
-  font-family: Poppins, sans-serif;
-  font-style: normal;
-  font-weight: normal;
-  font-size: 18px;
-  line-height: 27px;
+	font-family: Poppins, sans-serif;
+	font-style: normal;
+	font-weight: normal;
+	font-size: 18px;
+	line-height: 27px;
 
-  color: #19192a;
+	color: #19192a;
 
-  padding: 8px 14px;
+	padding: 8px 14px;
 }
 
 .inner button {
-  width: 90%;
+	width: 90%;
 
-  background: #e5e6eb;
-  border: none;
-  box-sizing: border-box;
-  border-radius: 8px;
-  outline: none;
+	background: #e5e6eb;
+	border: none;
+	box-sizing: border-box;
+	border-radius: 8px;
+	outline: none;
 
-  font-family: Poppins, sans-serif;
-  font-style: normal;
-  font-weight: 500;
-  font-size: 22px;
-  line-height: 33px;
+	font-family: Poppins, sans-serif;
+	font-style: normal;
+	font-weight: 500;
+	font-size: 22px;
+	line-height: 33px;
 
-  color: #19192a;
+	color: #19192a;
 
-  cursor: pointer;
+	cursor: pointer;
 }
 
 .hvr-grow {
-  transform: translateZ(0);
-  box-shadow: 0 0 1px rgba(0, 0, 0, 0);
-  backface-visibility: hidden;
-  -moz-osx-font-smoothing: grayscale;
-  transition-duration: 0.3s;
-  transition-property: transform;
+	transform: translateZ(0);
+	box-shadow: 0 0 1px rgba(0, 0, 0, 0);
+	backface-visibility: hidden;
+	-moz-osx-font-smoothing: grayscale;
+	transition-duration: 0.3s;
+	transition-property: transform;
 }
 
 .hvr-grow:hover,
 .hvr-grow:focus,
 .hvr-grow:active {
-  transform: scale(1.03);
-  box-shadow: 4px 4px 40px 4px #466ED1;
-  ;
+	transform: scale(1.03);
+	box-shadow: 4px 4px 40px 4px #466ed1;
 }
 
-@media(max-width:900px) {
-  .inner {
-    min-width: 90%;
-    width: initial;
-  }
-}</style>
+@media (max-width: 900px) {
+	.inner {
+		min-width: 90%;
+		width: initial;
+	}
+}
+</style>
