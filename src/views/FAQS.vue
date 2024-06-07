2a305b046fde2f2701ac4d979900910cdd0b07c1
ANIRUDH <149250431+ANIRUDHNARANG0@users.noreply.github.com>
2024-06-07 14:32:29 +0530

    feat(auth): Added login feature

---
+++ src/views/FAQS.vue
@@ -0,0 +1,13 @@
+<template>
+	<div :class="open ? 'faq.open' : 'faq'" @click="$emit('toggleOpen', index)">
+		<div class="question">{{ faq.question }}</div>
+		<div class="answer">{{ faq.answer }}</div>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'FAQ',
+	props: ['faq', 'index', 'open'],
+}
+</script>
